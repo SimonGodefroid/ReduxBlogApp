c1f3c4ca5e957f325ee2899e86ea3ddb80a7bc64
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-18 16:35:14 +0100

    wired action creator and containers to submit the form via a post request to the api

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
 class PostsNew extends Component {
 	renderField(field) {
 		const { meta: { touched, error } } = field;
@@ -15,6 +18,7 @@ class PostsNew extends Component {
 	}
 	onSubmit(values) {
 		console.log(values);
+		this.props.createPost(values);
 	}
 	render() {
 		const { handleSubmit } = this.props;
@@ -50,4 +54,4 @@ function validate(values) {
 export default reduxForm({
 	validate,
 	form: 'PostsNewForm'
-})(PostsNew);
+})(connect(null, { createPost })(PostsNew));
