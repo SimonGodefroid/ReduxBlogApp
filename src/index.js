8d541a442ca709b291f729c2a031ed833a5f5f63
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-18 16:57:59 +0100

    added route for PostsShow component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reducers from './reducers';
 
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
@@ -18,6 +19,7 @@ ReactDOM.render(
 				<Switch>
 					{/* After Switch we need to put the most specific routes first */}
 					<Route path={`/posts/new`} component={PostsNew} />
+					<Route path={`/posts/:id`} component={PostsShow} />
 					<Route path={`/`} component={PostsIndex} />
 				</Switch>
 			</div>
